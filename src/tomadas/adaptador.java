9e13ec809a010062ab0ab2ccaf8007eb77013f73
w7 <renan>
2014-06-16 17:26:12 -0300

    Atualização commit.

--- src/tomadas/adaptador.java
+++ src/tomadas/adaptador.java
@@ -7,19 +7,19 @@ package tomadas;
 public class adaptador {
 
     public static void main(String args[]) {
-// Criando tomadas.
+// Criando tomadas e flechas.
         FlechaN tomadaN = new FlechaN();
         FlechaV tomadaV = new FlechaV();
 // Ligar tomada velha com flecha velha.
-        tomadaV.insert("Tomada padrao velho.");
+        tomadaV.insert("Tomada padrão velho.");
 // Ligando tomada com adaptador.
         ITomadaV tomadaNSquare = new AdapterTomada(tomadaN);
-        tomadaNSquare.insert("Tomada padrao velho com adaptador.");
+        tomadaNSquare.insert("Tomada padrão velho com adaptador.");
 // Ligar tomada nova com flecha nova.
         tomadaN.insertIntoHole("Tomada padrão nova.");
 // Ligando tomada com adaptador.
         ITomadaN tomadaVToRound = new AdapterTomada(tomadaV);
-        tomadaVToRound.insertIntoHole("Tomada padrao novo com adaptador.");
+        tomadaVToRound.insertIntoHole("Tomada padrão novo com adaptador.");
     }
 
 }
